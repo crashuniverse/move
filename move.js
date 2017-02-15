4b67169f8c4d65aa1371d7edb1cc4adfbc0c8260
Priya Ranjan Singh <priya_ranjan_singh@mckinsey.com>
2017-02-15 11:02:32 +0530

    render 100 squares and move them 800 times

--- move.js
+++ move.js
@@ -1,12 +1,38 @@
-const square = document.querySelector('.square');
-let times = 0;
-const moveRight = () => {
-  ++times;
-  if (times === 5) {
-    window.clearInterval(interval);
+const NUMBER_OF_SQUARES = 100;
+const TIMES_MOVE = 800;
+const MOVE_BY_PX = 1;
+
+const drawSquares = (n) => {
+  for(let i = 0; i < n; i++) {
+    const e = document.createElement('div');
+    e.className = 'square';
+    e.id = 'square' + i;
+    const container = document.querySelector('.container');
+    container.appendChild(e);
+  }
+}
+
+drawSquares(NUMBER_OF_SQUARES);
+
+const squares = document.querySelectorAll('.square');
+const times = {};
+const intervals = {};
+
+const moveRight = (square, index) => {
+  times[index] = times[index] + 1;
+  if (times[index] === TIMES_MOVE) {
+    window.clearInterval(intervals[index]);
   }
   const margin = square.style.marginLeft.split('px')[0];
-  const newMargin = Number(margin) + 100;
+  const newMargin = Number(margin) + MOVE_BY_PX;
   square.style.marginLeft = newMargin + 'px';
 };
-const interval = window.setInterval(moveRight, 1000);
+
+console.time('prepareForRender');
+squares.forEach((square, index) => {
+  times[index] = 0;
+  intervals[index] = window.setInterval(() => {
+    moveRight(square, index);
+  }, 0);
+})
+console.timeEnd('prepareForRender');
