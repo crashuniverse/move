5b2b84871fbb00b62bce4c099c642c9a23f03e67
Priya Ranjan Singh <mail@priyaranjan.net>
2017-11-16 23:59:34 +0530

    introduce 2 box shadows

--- app.js
+++ app.js
@@ -2,17 +2,29 @@ const NUMBER_OF_SQUARES = 100;
 const TIMES_MOVE = 800;
 const MOVE_BY_PX = 1;
 
-const drawSquares = (n) => {
+const drawSquares = (n, color) => {
   for(let i = 0; i < n; i++) {
     const e = document.createElement('div');
-    e.className = 'square';
-    e.id = 'square' + i;
+    e.className = `square ${color}`;
+    e.id = `square_${color}_${i}`;
     const container = document.querySelector('.container');
     container.appendChild(e);
   }
 }
 
-drawSquares(NUMBER_OF_SQUARES);
+const drawShadows = (n) => {
+  const container = document.querySelector('.container');
+  const e = document.createElement('div');
+  e.className = `shadows`;
+  container.appendChild(e);
+  const shadows = document.querySelector('.shadows');
+  for(let i = 0; i < n; i++) {
+    shadows.style = 'box-shadow: rgba(192, 57, 43, 1) 0px 10px 0px 5px, rgba(192, 57, 43, 1) 0px 30px 0px 5px';
+  }
+}
+
+drawSquares(NUMBER_OF_SQUARES / 2, 'green');
+drawShadows(NUMBER_OF_SQUARES / 2);
 
 const squares = document.querySelectorAll('.square');
 const times = {};
